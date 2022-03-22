cff559134ad6c8fe385dd7247ba4895e8120acf2
hallgato <hallgato@LABOR210-04>
2022-03-22 13:02:14 +0100

    Logic crated and started

---
+++ MessageSenderApp.WpfClient/MessageSenderApp.Logic/Logic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageSenderApp.Logic
+{
+    class Logic : ILogic
+    {
+        public void ReciveMessage(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessage(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MessageSenderApp.WpfClient/MessageSenderApp.Logic/Message.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageSenderApp.Logic
+{
+    public class Message
+    {
+        private string messageValue;
+        private string name;
+        private string date;
+
+        public string MessageValue
+        {
+            get { return messageValue; }
+            set { messageValue = value; }
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+    }
+}
